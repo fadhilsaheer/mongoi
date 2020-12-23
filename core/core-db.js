52df44afbcd22039a2cfad8d5c634c196021ea94
fadhilsaheer <fadhilsaheer@gmail.com>
2020-12-23 19:15:45 +0530

    bug errors could'nt fixed

---
+++ core/core-db.js
@@ -0,0 +1,38 @@
+
+// here database is attaining connection and also create a schema 
+// for mongodb
+
+
+const mongoose = require("mongoose");
+
+module.exports = {
+
+    // connect database 
+
+    connectDatabase:(connectUrl)=>{
+        return new Promise((resolve, reject)=>{
+            mongoose.connect(connectUrl, { useNewUrlParser:true, useUnifiedTopology:true }, ()=>{
+                console.log(`[INFO] Database connected to ${connectUrl}`)
+                resolve()
+            });
+        })
+    },
+
+    // get all deatils about database schema
+
+    getDatabaseSchema:(databaseItems)=>{
+        return new Promise((resolve, reject)=>{
+            const databaseSchema = new mongoose.Schema(databaseItems);
+            resolve(databaseSchema) // exporting database schema
+        });
+    },
+
+    // create database model
+
+    createDatabaseModel:(databaseSchema, collectionName)=>{
+        return new Promise(async(resolve, reject)=>{
+            const databaseModel = await new mongoose.model(collectionName, databaseSchema);
+            resolve(databaseModel);
+        });
+    }
+}
--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-// Database module
 
+
+const coreDatabase = require('./core/core-db');
+
+// This function will connect to database and create a db schema
+
+function init(connectionUrl, databaseSchema, collectionName){
+    coreDatabase.connectDatabase(connectionUrl);
+    coreDatabase.getDatabaseSchema(databaseSchema).then((schema)=>{
+        console.log("hello world")
+        coreDatabase.createDatabaseModel(schema, collectionName).then((model)=>{
+            console.log(model)
+        })
+    })
+}
+
+let schem = {
+    first_name: String,
+    last_name: String
+}
+init("mongodb://localhost:27017/mongoi", schem, "users")
