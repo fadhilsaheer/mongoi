b5d273a92f6a6d47e1596a1cb9dcfcbf133a4916
fadhilsaheer <fadhilsaheer@gmail.com>
2021-01-22 17:12:25 +0530

    removed bug test function

--- core/core-db.js
+++ core/core-db.js
@@ -1,50 +1,42 @@
-
-// here database is attaining connection and also create a schema 
+// here database is attaining connection and also create a schema
 // for mongodb
-
 
 const mongoose = require("mongoose");
 
 module.exports = {
+  // connect database
 
-    // connect database 
+  connectDatabase: (connectUrl) => {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(
+        connectUrl,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => {
+          console.log(`[INFO] Database connected to ${connectUrl}`);
+          resolve();
+        }
+      );
+    });
+  },
 
-    connectDatabase:(connectUrl)=>{
-        return new Promise((resolve, reject)=>{
-            mongoose.connect(connectUrl, { useNewUrlParser:true, useUnifiedTopology:true }, ()=>{
-                console.log(`[INFO] Database connected to ${connectUrl}`)
-                resolve()
-            });
-        })
-    },
+  // get all deatils about database schema
 
-    // get all deatils about database schema
+  getDatabaseSchema: (databaseItems) => {
+    return new Promise((resolve, reject) => {
+      const databaseSchema = new mongoose.Schema(databaseItems);
+      resolve(databaseSchema); // exporting database schema
+    });
+  },
 
-    getDatabaseSchema:(databaseItems)=>{
-        return new Promise((resolve, reject)=>{
-            const databaseSchema = new mongoose.Schema(databaseItems);
-            resolve(databaseSchema) // exporting database schema
-        });
-    },
+  // create database model
 
-    // create database model
-
-    createDatabaseModel:(databaseSchema, collectionName)=>{
-        return new Promise(async(resolve, reject)=>{
-            const databaseModel = await new mongoose.model(collectionName, databaseSchema);
-            resolve(databaseModel);
-        });
-    },
-
-    // For a bug test 
-
-    // model is database-model and data is the data it should be an object
-    savData:(model, data)=>{
-        return new Promise((resolve, reject)=>{
-            let newData = new model(data);
-            newData.save();
-            resolve();
-        })
-    }
-
-}
+  createDatabaseModel: (databaseSchema, collectionName) => {
+    return new Promise(async (resolve, reject) => {
+      const databaseModel = await new mongoose.model(
+        collectionName,
+        databaseSchema
+      );
+      resolve(databaseModel);
+    });
+  },
+};
